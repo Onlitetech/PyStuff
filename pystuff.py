3c80227debbe58425fc6d7145e39b47fa657c941
Aiden <194514@gmail.com>
2011-11-23 18:14:23 -0700

    start

---
+++ pystuff.py
@@ -0,0 +1,8 @@
+from string import maketrans
+def rot13(str):
+	intab = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	outtab = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
+	trantab = maketrans(intab, outtab)
+	transs=str.translate(trantab);
+	return transs
+print rot13("sup")
