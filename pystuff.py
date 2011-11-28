2abfab5e173d753900ea17a5770966f73ed16535
Aiden <194514@gmail.com>
2011-11-28 00:25:00 -0500

    Add lsf to pystuff.py

--- pystuff.py
+++ pystuff.py
@@ -5,3 +5,14 @@ def rot13(str):
 	trantab = maketrans(intab, outtab)
 	transs=str.translate(trantab);
 	return transs
+def lsf(s):
+	f=open(s,'r')
+	words=0
+	lines=0
+	chars=0
+	for line in f:
+		words +=len(split(line))
+		for x in split(line):
+			chars +=len(x)
+		lines +=1
+	return "words: " + str(words) + " Lines: " + str(lines) + " Characters: " + str(chars)
