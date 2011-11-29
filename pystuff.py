0649e75267843b9b20b88f4e26b426aca6765abe
Aiden <194514@gmail.com>
2011-11-28 20:34:26 -0500

    Updated pystuff.py with palin function, updated lsf function with vowels and consanants. Renamed lsf to fs

--- pystuff.py
+++ pystuff.py
@@ -5,14 +5,53 @@ def rot13(str):
 	trantab = maketrans(intab, outtab)
 	transs=str.translate(trantab);
 	return transs
-def lsf(s):
+def fs(s):
 	f=open(s,'r')
 	words=0
 	lines=0
 	chars=0
+	vowels=0
+	cons=0
+	vowels = ["a","A","e","E","i","I","o","O","u","U"]
+	a = 0 #Current letter
+	vow = 0
+	cons = 0
+	str = f.read()
+	for x in str:
+		if str[a] == x in vowels:
+			vow +=1
+		else:
+			cons +=1
+		a +=1
+
+	if vow == 0:
+		if "y" or "Y" in str[a]:
+			vow +=1
+			cons -=1 #remove 1 from cons, because the y has already been counted as a consonant.
+	
 	for line in f:
 		words +=len(split(line))
 		for x in split(line):
 			chars +=len(x)
 		lines +=1
-	return "words: " + str(words) + " Lines: " + str(lines) + " Characters: " + str(chars)
+	return "words: " + str(words) + " Lines: " + str(lines) + " Characters: " + str(chars) + "Vowels: " + str(vows) + "Consanants: " + str(cons)
+def palin(str):
+	a = len(str) # length of string
+	b = a / 2 # half of string length
+	c = 0 #Current letter in str
+	prestring = [] #first half of string
+	endstring = [] #last half of string
+	while b > len(prestring):
+		prestring.append(str[c])
+		c += 1
+		c += 1
+
+	while b > len(endstring):
+		endstring.append(str[c])
+		c += 1
+	
+	endstring = endstring[::-1]
+	if prestring == endstring:
+		return "Palindrome"
+	else:
+		return "Not a palindrome"
