e5be7787eb8c0df4243434021d9575aa1d6cc97c
Aiden <194514@gmail.com>
2011-11-29 22:11:32 -0700

    Added setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='PyStuff',
+      version='0.1',
+      description='Python Function Library',
+      author='Aiden Eckford',
+      author_email='194514@gmail.com',
+      url='https://github.com/DLG-HQ/PyStuff',
+      packages=['pystuff'],
+     )
